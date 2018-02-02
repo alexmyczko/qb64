a4df0b985b6ba39a6550248072630b42211c146d
Luke Ceddia <flukiluke@gmail.com>
2018-02-02 16:28:20 +1100

    Improve temporary build script

--- internal/c/libqb/build.sh
+++ internal/c/libqb/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd libqb
-for file in $(ls src)
-    do g++ "$@" -c -s -w -Wall -I .. -I include src/$file -o os/lnx/${file%.cpp}.o
+
+for file in $(find src -name '*.cpp')
+    do g++ "$@" -c -s -w -Wall -I .. -I include -I include/$(dirname $file) $file -o os/lnx/$(basename ${file%.cpp}.o)
 done
 
 ar rcs os/lnx/libqb.a $(ls os/lnx/*.o)
